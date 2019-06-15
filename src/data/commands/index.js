98581775bd7c87c07a71198548ac9ed8fd9867fd
Pyroarsonist <velikiydan@gmail.com>
2019-06-15 15:50:59 +0300

    added notes editor

--- src/data/commands/index.js
+++ src/data/commands/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import { bot } from 'core/telegram';
 import debugHandler from 'debug';
+import session from 'telegraf/session';
 import userMiddleware from './userMiddleware';
 import noteCommand from './noteCommand';
 
@@ -11,12 +12,9 @@ export default () => {
     debug(err);
   });
 
+  bot.use(session());
+
   bot.use(userMiddleware);
 
   noteCommand(bot);
-  // bot.command('addNode', noteCommand);
-
-  bot.command('start', async ctx =>
-    ctx.reply('Привет! Ты можешь добавить новые записки или изменить старые!'),
-  );
 };
--- src/data/commands/noteCommand.js
+++ src/data/commands/noteCommand.js
@@ -1,82 +1,91 @@
-// import debugHandler from 'debug';
 import TelegrafInlineMenu from 'telegraf-inline-menu';
+import { db } from 'core/db';
 
-// const Telegraf = require('telegraf')
-// const Composer = require('telegraf/composer')
-// const session = require('telegraf/session')
-// const Stage = require('telegraf/stage')
-// const Markup = require('telegraf/markup')
-// const WizardScene = require('telegraf/scenes/wizard')
-//
-// const stepHandler = new Composer()
-// stepHandler.action('next', (ctx) => {
-//   ctx.reply('Step 2. Via inline button')
-//   return ctx.wizard.next()
-// })
-// stepHandler.command('next', (ctx) => {
-//   ctx.reply('Step 2. Via command')
-//   return ctx.wizard.next()
-// })
-// stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button or type /next'))
-//
-// const superWizard = new WizardScene('super-wizard',
-//   (ctx) => {
-//     ctx.reply('Step 1', Markup.inlineKeyboard([
-//       Markup.urlButton('❤️', 'http://telegraf.js.org'),
-//       Markup.callbackButton('➡️ Next', 'next')
-//     ]).extra())
-//     return ctx.wizard.next()
+const menu = new TelegrafInlineMenu('Редактор записок');
+const replyMiddleware = menu.replyMenuMiddleware();
+
+const subMenu = new TelegrafInlineMenu(() => 'Выберите, что делать с запиской');
+// subMenu.simpleButton('Редактировать', 'edit', {
+//   questionText: 'Новый текст',
+//   setFunc: async ctx => {
+//     const text = ctx.match[1];
+//     const w = await db
+//       .collection('notes')
+//       .where('text', '==', text)
+//       .get();
+//     w.forEach(async x => {
+//       if (x) {
+//         await db
+//           .collection('notes')
+//           .doc(x.id)
+//           .update({
+//             text: 's',
+//           });
+//       }
+//     });
+//     await ctx.reply('Сохранено!');
+//     replyMiddleware.setSpecific(ctx, 'main');
 //   },
-//   stepHandler,
-//   (ctx) => {
-//     ctx.reply('Step 3')
-//     return ctx.wizard.next()
-//   },
-//   (ctx) => {
-//     ctx.reply('Step 4')
-//     return ctx.wizard.next()
-//   },
-//   (ctx) => {
-//     ctx.reply('Done')
-//     return ctx.scene.leave()
-//   }
-// )
-//
-// const stage = new Stage([superWizard], { default: 'super-wizard' })
-
-// const debug = debugHandler('exam:commands:note-command');
-
-const startMenu = new TelegrafInlineMenu(
-  ctx =>
-    `Привет, ${
-      ctx.user.firstName
-    }! Ты можешь добавить новые записки или изменить старые!`,
-);
-// const createMenu = new TelegrafInlineMenu(ctx => 'Создаем новую записку');
-// const readMenu = new TelegrafInlineMenu(ctx => 'Показываем все записки');
-// const editMenu = new TelegrafInlineMenu(ctx => 'Редактируем записку');
-// const deleteMenu = new TelegrafInlineMenu(ctx => 'Удаляем записку');
-
-startMenu.question('Создать новую записку', 'cr', {
-  questionText: 'Введите заголовок записки',
-  setFunc: async (ctx, key) => ctx.reply(`Вы вписали ${key}`),
-  joinLastRow: true,
-});
-// .question('кек', 'sdsd', {
-//   questionText: 'c2',
-//   setFunc: async (ctx, key) => ctx.reply(`Вы выбрали2 ${key}`),
-//   joinLastRow: true,
 // });
-// startMenu.submenu('Создать новую записку', 'c', createMenu);
-// startMenu.submenu('Показать все записки', 'r', readMenu);
-// startMenu.submenu('Редакактировать записку', 'e', editMenu);
-// startMenu.submenu('Удалить записку', 'd', deleteMenu);
-//
-startMenu.setCommand('start');
+
+subMenu.simpleButton('Удалить', 'del', {
+  doFunc: async ctx => {
+    const text = ctx.match[1];
+    const w = await db
+      .collection('notes')
+      .where('text', '==', text)
+      .get();
+    w.forEach(async x => {
+      if (x) {
+        await db
+          .collection('notes')
+          .doc(x.id)
+          .delete();
+      }
+    });
+    await ctx.reply('Удалено!');
+    replyMiddleware.setSpecific(ctx, 'main');
+  },
+});
+
+menu.selectSubmenu(
+  'main',
+  async ctx => {
+    const notes = [];
+    const notesQuery = await db
+      .collection('notes')
+      .where('ownerId', '==', ctx.user.telegramId)
+      .get();
+    notesQuery.forEach(x => {
+      // if (x) notes.push({ ...x.data(), id: x.id });
+      if (x) notes.push(x.data().text);
+    });
+    return notes;
+  },
+  subMenu,
+  {
+    // textFunc: (_ctx, note) => note.text,
+    columns: 4,
+  },
+);
+
+menu.question('Добавить записку', 'add', {
+  questionText: 'Новая записка',
+  setFunc: async (ctx, key) => {
+    await db.collection('notes').add({
+      text: key,
+      ownerId: ctx.user.telegramId,
+    });
+  },
+});
+
+menu.setCommand('start');
 
 export default bot => {
-  // bot.use(session());
-  // bot.use(stage.middleware());
-  // bot.launch()
-  bot.use(startMenu.init());
+  bot.use(
+    menu.init({
+      backButtonText: 'Назад',
+      mainMenuButtonText: 'Назад в главное меню',
+    }),
+  );
 };
